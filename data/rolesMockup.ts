b84864bec01d469eb274415e5751b9525e23cb29
izmi.komar <komar.izmi@gmail.com>
2024-04-15 02:02:21 +0700

    Update role names and add timestamps in rolesMockup.ts and usersMockup.ts

--- data/rolesMockup.ts
+++ data/rolesMockup.ts
@@ -1,22 +1,25 @@
 import { faker } from "@faker-js/faker";
 export type Roles = {
   id: string;
-  roleName: string;
-  createdAt: Date | string;
+  role_name: string;
+  created_at: Date | string;
+  updated_at: Date | string;
+  deleted_at?: Date | string;
 };
 const roles = Array.from({ length: 10 }, () => ({
-  roleName: faker.person.jobType(),
+  role_name: faker.person.jobType(),
 }));
-roles.push({ roleName: "Administrator" });
-const uniqueRoles = new Set(roles.map((item) => item.roleName));
+roles.push({ role_name: "Administrator" });
+const uniqueRoles = new Set(roles.map((item) => item.role_name));
 const rolesMockup: Roles[] = Array.from(uniqueRoles)
   .map((item) => ({
     id: crypto.randomUUID(),
-    roleName: item,
-    createdAt: faker.date.past(),
+    role_name: item,
+    created_at: faker.date.past(),
+    updated_at: faker.date.recent(),
   }))
   .sort((a, b) =>
-    a.roleName.toLowerCase().localeCompare(b.roleName.toLowerCase())
+    a.role_name.toLowerCase().localeCompare(b.role_name.toLowerCase())
   );
 
 export { rolesMockup };
--- data/usersMockup.ts
+++ data/usersMockup.ts
@@ -1,21 +1,26 @@
 import { faker } from "@faker-js/faker";
-import { Roles } from "./rolesMockup";
 import { rolesMockup } from "./rolesMockup";
 import { randomNumbers } from "@/lib/utils";
+import * as bcrypt from "bcrypt";
+import { SALT_ROUNDS } from "@/config/app";
 export type UserType = {
   id: string;
-  firstName: string;
-  lastName: string;
+  name: string;
   email: string;
-  createdAt: Date | string;
-  role: Roles;
+  password: string;
+  role_id: string;
+  created_at: Date | string;
+  updated_at: Date | string;
+  deleted_at?: Date | string | null;
 };
 const usersMockup: UserType[] = Array.from({ length: 100 }, () => ({
   id: crypto.randomUUID(),
-  firstName: faker.person.firstName(),
-  lastName: faker.person.lastName(),
+  name: faker.person.fullName(),
   email: faker.internet.email(),
-  createdAt: faker.date.past(),
-  role: rolesMockup[randomNumbers(0, rolesMockup.length)],
+  password: bcrypt.hashSync("password", SALT_ROUNDS),
+  role_id: rolesMockup[randomNumbers(0, rolesMockup.length - 1)].id,
+  created_at: faker.date.recent(),
+  updated_at: faker.date.recent(),
+  deleted_at: null,
 }));
 export { usersMockup };
