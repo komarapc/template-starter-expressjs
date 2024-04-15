f224931e82f07dd29b15d469ed30beea0f7d93ca
izmi.komar <komar.izmi@gmail.com>
2024-04-15 14:07:19 +0700

    Add SECRET_KEY to app.ts configuration file

--- config/app.ts
+++ config/app.ts
@@ -2,4 +2,5 @@ import dotenv from "dotenv";
 dotenv.config();
 const PORT: number = Number(process.env.PORT) || 8000;
 const SALT_ROUNDS: number = Number(process.env.SALT_ROUNDS) || 10;
-export { PORT, SALT_ROUNDS };
+const SECRET_KEY = process.env.SECRET_KEY;
+export { PORT, SALT_ROUNDS, SECRET_KEY };
--- src/tokens/tokens.service.ts
+++ src/tokens/tokens.service.ts
@@ -1,7 +1,8 @@
 import { responseError, responseSuccess } from "@/lib/utils";
 import TokenRepo, { Tokens } from "./tokens.repo";
 import { validateTokenInput } from "./tokens.validation";
-
+import jwt from "jsonwebtoken";
+import { SECRET_KEY } from "@/config/app";
 class TokenService {
   private readonly tokenRepo: TokenRepo = new TokenRepo();
   async findAll() {
@@ -30,6 +31,10 @@ class TokenService {
     try {
       const errors = validateTokenInput(token);
       if (errors.length) return responseError({ code: 400, message: errors });
+      const jwtToken = this.generateToken({ user_id: token.user_id });
+      if (!jwtToken)
+        return responseError({ code: 500, message: "Can not create token" });
+      token.token = jwtToken;
       const storeToken = await this.tokenRepo.store(token);
       return responseSuccess({ code: 201, data: storeToken });
     } catch (error) {
@@ -48,6 +53,15 @@ class TokenService {
       });
     } catch (error) {
       return responseError({ code: 500 });
+    }
+  }
+  private generateToken(payload: any) {
+    try {
+      if (!payload || SECRET_KEY) return false;
+      const token = jwt.sign(payload, SECRET_KEY!, { expiresIn: "30d" });
+      return token;
+    } catch (error) {
+      return false;
     }
   }
 }
