804e9696510af6df681166bdd2036f54673b871a
izmi.komar <komar.izmi@gmail.com>
2024-04-16 11:32:17 +0700

    Add DEBUG flag to app.ts configuration file

--- config/app.ts
+++ config/app.ts
@@ -1,6 +1,8 @@
+import { stringToBoolean } from "@/lib/utils";
 import dotenv from "dotenv";
 dotenv.config();
 const PORT: number = Number(process.env.PORT) || 8000;
 const SALT_ROUNDS: number = Number(process.env.SALT_ROUNDS) || 10;
 const SECRET_KEY = process.env.SECRET_KEY;
-export { PORT, SALT_ROUNDS, SECRET_KEY };
+const DEBUG = stringToBoolean(String(process.env.DEBUG));
+export { PORT, SALT_ROUNDS, SECRET_KEY, DEBUG };
