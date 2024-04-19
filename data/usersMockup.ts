817f58f1cf0768b540f52f3de6036b0c77fef106
izmi.komar <komar.izmi@gmail.com>
2024-04-20 00:10:52 +0700

    Refactor userMockup.ts to include UserNoPasswordField type

--- data/usersMockup.ts
+++ data/usersMockup.ts
@@ -13,6 +13,15 @@ export type UserType = {
   updated_at: Date | string;
   deleted_at?: Date | string | null;
 };
+export type UserNoPasswordField = {
+  id: string;
+  name: string;
+  email: string;
+  role_id: string;
+  created_at: Date | string;
+  updated_at: Date | string;
+  deleted_at?: Date | string | null;
+};
 const usersMockup: UserType[] = Array.from({ length: 100 }, () => ({
   id: crypto.randomUUID(),
   name: faker.person.fullName(),
